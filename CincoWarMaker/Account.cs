9e3386c197885cfcef8fbe96ab657d10913f1775
ROBOTEK9\deadringer21 <deadringer21@gmail.com>
2018-11-15 19:44:12 -0500

    Merge branch 'ACCT_OBJ' of https://www.github.com/pmk724/OUTWAR
    # Conflicts:
    #	.vs/cincowarmaker/v15/.suo
    
    Added Account.cs with a simple constructor to create Account objects.

---
+++ CincoWarMaker/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CincoWarMaker
+{
+	class Account
+	{
+		private string name, crew;
+		private int level, suid;
+		private long exp, power;
+		
+		public Account(string name, int level, string crew, long exp, long power, int suid)
+		{
+			this.name = name;
+			this.level = level;
+			this.crew = crew;
+			this.exp = exp;
+			this.power = power;
+			this.suid = suid;
+		}
+	}
+}
