6c32a966dee0941474d6485d28ad1d6924118e32
ROBOTEK9\deadringer21 <deadringer21@gmail.com>
2018-11-15 20:12:47 -0500

    Added alternate constructor for a quick-read of accounts on the RGA

--- CincoWarMaker/Account.cs
+++ CincoWarMaker/Account.cs
@@ -11,7 +11,31 @@ namespace CincoWarMaker
 		private string name, crew;
 		private int level, suid;
 		private long exp, power;
-		
+
+		/// <summary>
+		/// Account(name,suid)
+		/// Used for a quick-read of the accounts on the RGA
+		/// Source page is http://www.outwar.com/ajax/accounts.php
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="suid"></param>
+		public Account(string name, int suid)
+		{
+			this.name = name;
+			this.suid = suid;
+		}
+
+		/// <summary>
+		/// Account(name, level, crew, exp, power, suid)
+		/// Used for a full read of Accounts including extra details not included in the two-param constructor
+		/// Source page is http://www.outwar.com/myaccount.php
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="level"></param>
+		/// <param name="crew"></param>
+		/// <param name="exp"></param>
+		/// <param name="power"></param>
+		/// <param name="suid"></param>
 		public Account(string name, int level, string crew, long exp, long power, int suid)
 		{
 			this.name = name;
