3f6bdfa9c41ce23f91f04d6652f8c5a6e57e85c1
ROBOTEK9\deadringer21 <deadringer21@gmail.com>
2018-11-15 23:39:05 -0500

    Added basic logic (comments) to Account.cs to help with reading account stats for trustee accounts

--- CincoWarMaker/Account.cs
+++ CincoWarMaker/Account.cs
@@ -46,5 +46,27 @@ namespace CincoWarMaker
 			this.power = power;
 			this.suid = suid;
 		}
+
+		/// <summary>
+		/// Since trustee accounts don't include Power/Exp/Rage on myaccount.php, the stats must be read individually
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public async Task readStats()
+		{
+			/*
+			 * home.php?suid=12345 displays detailed stats - REQUIRES switching to the specified account to review home data
+			 * There are five <div id="divPlayerStats"> nodes:
+			 *	 The third includes detailed elemental damage, and the fourth includes detailed elemental resistance
+			 *
+			 * profile.php?suid=12345 displays Power, Atk, HP, totalElements, totalResists
+			 *	 Power: #divPlayerInfo > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > b:nth-child(1) > font:nth-child(1)
+			 *	 Atk: #divPlayerInfo > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2) > b:nth-child(1) > font:nth-child(1)
+			 *	 HP: #divPlayerInfo > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(2) > b:nth-child(1) > font:nth-child(1)
+			 *	 totalElements: #divPlayerInfo > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(2) > b:nth-child(1) > font:nth-child(1)
+			 *	 totalResists: #divPlayerInfo > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(8) > td:nth-child(2) > b:nth-child(1) > font:nth-child(1)
+			 * 
+			 */
+		}
 	}
 }
